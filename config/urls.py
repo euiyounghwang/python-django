53a2dc1243edfaa14b6ac1f7777ed689b94a3be4
euiyounghwang <marieuig@gmail.com>
2023-10-25 14:07:08 -0500

    Update rest_ui

--- config/urls.py
+++ config/urls.py
@@ -45,11 +45,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path(r'^admin/$', admin.site.urls),
     # --
-    # Django REST Framework (DRF), 
-    # --
+    # User Interface App
     path('rest_api/', include(('rest_api.urls', 'rest_api'))),
+    # --
+    # Django REST Framework (DRF), 
     path('rest_ui/', include(('rest_ui.urls', 'rest_ui'))),
     path('book_rest_api/', include(('book_rest_api.urls', 'book_rest_api'))),
+    # --
     # path("prometheus/", include("django_prometheus.urls"))
 ]
 
