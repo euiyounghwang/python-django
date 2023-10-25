896bbeef57f360cd65b7ed32fb2cfa14e9a11050
euiyounghwang <marieuig@gmail.com>
2023-10-25 14:06:18 -0500

    Update rest_ui

--- rest_ui/views.py
+++ rest_ui/views.py
@@ -5,6 +5,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from rest_api.models import Student
+from rest_api.injector import logger
+
 def rest_apis(request):
+  '''
+  # Default index.html
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
+  '''
+  students = Student.objects.all().values()
+  logger.info("students : {}".format(students))
+  template = loader.get_template('all_students.html')
+  context = {
+    'students': students,
+  }
+  return HttpResponse(template.render(context, request))
+
