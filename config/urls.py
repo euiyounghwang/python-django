e4497368162d54fcc8f7fc2242acc7f5d6f22f60
euiyounghwang <marieuig@gmail.com>
2023-10-31 22:27:13 -0500

    Update pytest

--- config/urls.py
+++ config/urls.py
@@ -45,6 +45,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path(r'^admin/$', admin.site.urls),
     # --
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # User Interface App
     path('rest_ui/', include(('rest_ui.urls', 'rest_ui'))),
     # --
@@ -56,9 +57,9 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    ]
+# if settings.DEBUG:
+#     urlpatterns += [
+#         re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+#         re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#         re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+#     ]
