59dd648c64f693c07ee5b41da86abd5a175dd73d
euiyounghwang <marieuig@gmail.com>
2023-11-06 22:25:04 -0600

    Update Indexing-db-Script

--- tools/Search-indexing-db-script.py
+++ tools/Search-indexing-db-script.py
@@ -6,8 +6,10 @@ import json
 from elasticsearch import Elasticsearch
 import argparse
 import psycopg2
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 load_dotenv()
 
@@ -18,16 +20,24 @@ class Databases():
     
     def __init__(self):
         self.db = psycopg2.connect(os.getenv("DATABASE_URL", "postgresql://postgres:1234@{}:{}/postgres".format("localhost", 15432)))
-        self.cursor = self.db.cursor()
+        self.cursor = self.db.cursor(cursor_factory=RealDictCursor)
 
     # def __del__(self):
     #     self.db.close()
     #     self.cursor.close()
 
+    def datetime_handler(self, x):
+        if isinstance(x, datetime):
+                return x.strftime('%Y-%m-%d %H:%M:%S')
+        raise TypeError("Unknown Type")
+
     def execute(self,query,args={}):
-        self.cursor.execute(query,args)
-        row = self.cursor.fetchall()
-        return row
+        # self.cursor.execute(query,args)
+        self.cursor.execute(query)
+        data = self.cursor.fetchall()
+        rows = [dict(row) for row in data]
+        print(json.dumps(rows, indent=2, default=self.datetime_handler))
+        return rows
 
     def commit(self):
         self.cursor.commit()
@@ -36,6 +46,69 @@ class Databases():
         self.db.close()
         self.cursor.close()
         print("Closed successfully!!!") 
+
+
+class Search():
+    ''' elasticsearch class '''
+    
+    def __init__(self, host):
+        self.timeout = 60
+        self.es_client = Elasticsearch(hosts=host, headers=self.get_headers(), timeout=self.timeout)
+    
+    
+    def get_headers(self):
+        ''' Elasticsearch Header '''
+        return {
+            'Content-type': 'application/json', 
+            'Connection': 'close'
+        }
+        
+    
+    def Get_Buffer_Length(self, docs):
+        """
+        :param docs:
+        :return:
+        """
+        max_len = 0
+        for doc in docs:
+            max_len += len(str(doc))
+
+        return max_len
+    
+        
+    def create_index(self, _index):
+        print(self.es_client)
+        mapping = {
+            "mappings": {
+                "properties": {
+                    "title": {
+                        "type": "text",
+                        "analyzer": "standard",
+                        "fields": {
+                            "keyword": {
+                                "type": "keyword"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        try:
+            if self.es_client.indices.exists(index=_index):
+                self.es_client.indices.delete(index=_index, ignore=[400, 404])
+                print("Successfully deleted: {}".format(_index))
+
+            print('Creating..')
+            # now create a new index
+            self.es_client.indices.create(index=_index, body=mapping)
+            # es_client.indices.put_alias(index, "omnisearch_search")
+            self.es_client.indices.refresh(index=_index)
+            print("Successfully created: {}".format(_index))
+            
+        except Exception as error:
+            print('Error: {}, index: {}'.format(error, _index))
+
 
 
 def get_db_connection():
@@ -47,64 +120,43 @@ def get_db_connection():
     except Exception as e:
         print("mongodb_connection - {}".format(str(e)))
         
-def get_headers():
-    ''' Elasticsearch Header '''
-    return {'Content-type': 'application/json', 'Connection': 'close'}
-
-
-def get_es_instance(_host):
-    # create a new instance of the Elasticsearch client class
-    es_client = Elasticsearch(hosts=_host, headers=get_headers(), timeout=5)
-    return es_client
-
-def create_index(es_client, _index):
-    print(es_client)
-    mapping = {
-        "mappings": {
-            "properties": {
-                "title": {
-                    "type": "text",
-                    "analyzer": "standard",
-                    "fields": {
-                        "keyword": {
-                            "type": "keyword"
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    try:
-        if es_client.indices.exists(index=_index):
-            es_client.indices.delete(index=_index, ignore=[400, 404])
-            print("Successfully deleted: {}".format(_index))
-
-        print('Creating..')
-        # now create a new index
-        es_client.indices.create(index=_index, body=mapping)
-        # es_client.indices.put_alias(index, "omnisearch_search")
-        es_client.indices.refresh(index=index)
-        print("Successfully created: {}".format(_index))
-    except Exception as error:
-        print('Error: {}, index: {}'.format(error, _index))
-
-
-def Get_Buffer_Length(docs):
-    """
-    :param docs:
-    :return:
-    """
-    max_len = 0
-    for doc in docs:
-        max_len += len(str(doc))
-
-    return max_len
-
-
-
+        
 if __name__ == "__main__":
+    
+    ''' 
+        https://edudeveloper.tistory.com/131
+        Run (Postgresql DB Select and Indexing into Elasticsearch, Scripts)
+        poetry run python ./tools/Search-indexing-db-script.py --es=http://localhost:9209  --index=search_indexing-db 
+        poetry run python ./search-indexing-script.py --es $ES_HOST --DATABASE_URL $DATABASE_URL --index $INDEX_NAME (Docker Based)
+    '''
+    parser = argparse.ArgumentParser(description="Index into Elasticsearch using this script")
+    parser.add_argument('-e', '--es', dest='es', default="http://localhost:9209", help='host target')
+    parser.add_argument('-i', '--index', dest='index', default="search_indexing-db", help='host target')
+    args = parser.parse_args()
+    
+    if args.es:
+        es_host = args.es
+        
+    if args.index:
+        es_index_name = args.index
+        
+    es_client, client = None, None
     try:
-        client = get_db_connection()
+        client = Databases()
+        if client:
+            print("Connected successfully!!!")
+            
+        ''' offset == size, limit == paging number '''
+        client.execute(
+                query='SELECT * from {} LIMIT 10 OFFSET 0'.format('public.student'),
+                
+        )
+        
+        # es_client = Search(host=es_host)
+        # es_client.create_index(_index=es_index_name)
+            
+    except Exception as e:
+        print("Connection - {}".format(str(e)))
+        
     finally:
         client.close()
--- tools/Search-pymongo-script.py
+++ tools/Search-pymongo-script.py
@@ -5,68 +5,81 @@ from pymongo import MongoClient
 import json
 
 
-def mongodb_connection():
-    ''' mongodb connection '''
+class Databases():
+    
+    def __init__(self):
+        self.client = MongoClient("mongodb://postgres:1234@localhost:27017/")
+    
+    
+    def Select(self, db, collection):
+        try:
+            #Getting the database instance
+            db = self.client[db]
+
+            #Creating a collection
+            coll = db[collection]
+
+            cursor = coll.find({})
+            for document in cursor:
+                print(json.dumps(document, indent=2))
+
+        except Exception as e:
+            print(str(e))
+     
+     
+    def Insert(self, db, collection, data):
+        ''' delete collection and insert '''
+        try:
+            # Getting the database instance
+            db = self.client[db]
+
+            # Creating a collection
+            coll = db[collection]
+            
+            res = db[collection].drop()
+            
+            res = coll.insert_many(data)
+            print("Data inserted ......")
+            print(res.inserted_ids)
+
+        except Exception as e:
+            print(str(e))
+            
+        
+    def close(self):
+        self.client.close()
+        print("Closed successfully!!!") 
+
+
+def get_db_connection():
+    ''' postgres connection '''
     try: 
-        conn = MongoClient("mongodb://postgres:1234@localhost:27017/") 
+        client = Databases()
         print("Connected successfully!!!") 
-        return conn
+        return client
     except Exception as e:
-        print("mongodb_connection - {}".format(str(e)))
+        print("get_db_connection - {}".format(str(e)))
+        
+       
 
-
-def mongodb_insert(client):
-    ''' delete collection and insert '''
+     
+if __name__ == "__main__":
+   
     try:
-        # Getting the database instance
-        db = client['local']
-
-        # Creating a collection
-        coll = db['example']
-        
-        res = db['example'].drop()
-        
-        # Inserting document into a collection
+         # Inserting document into a collection
         '''
-        doc1 = {"name": "Ram", "age": "26", "city": "Hyderabad"}
-        coll.insert_one(doc1)
-        print(coll.find_one())
+            doc1 = {"name": "Ram", "age": "26", "city": "Hyderabad"}
+            coll.insert_one(doc1)
+            print(coll.find_one())
         '''
         data = [
             {"_id": "101", "name": "Ram", "age": "26", "city": "Hyderabad"},
             {"_id": "102", "name": "Rahim", "age": "27", "city": "Bangalore", "addr" : "addr"},
             {"_id": "103", "name": "Robert", "age": "28", "city": "Mumbai"}
         ]
-        
-        res = coll.insert_many(data)
-        print("Data inserted ......")
-        print(res.inserted_ids)
-
-    except Exception as e:
-        print(str(e))
-        
-        
-def mongodb_select(client):
-    try:
-        #Getting the database instance
-        db = client['local']
-
-        #Creating a collection
-        coll = db['example']
-
-        cursor = coll.find({})
-        for document in cursor:
-            print(json.dumps(document, indent=2))
-
-    except Exception as e:
-        print(str(e))
-     
-     
-if __name__ == "__main__":
-   
-    try:
-        client = mongodb_connection()
-        mongodb_insert(client)
-        mongodb_select(client)
+                        
+        client = get_db_connection()
+        client.Insert(db='local', collection='example', data=data)
+        client.Select(db='local', collection='example')
     finally:
         client.close()
